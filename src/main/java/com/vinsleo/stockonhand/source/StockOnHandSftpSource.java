b5d1f7d25b5e1802aa87bf2ed88f7a74ec6bacb1
Vinay Thakkar <vinay.thakkar@argos.co.uk>
2018-08-15 01:28:38 +0100

    fixed the issue by creating additional Channel. No Wiretap

--- src/main/java/com/vinsleo/stockonhand/source/StockOnHandSftpSource.java
+++ src/main/java/com/vinsleo/stockonhand/source/StockOnHandSftpSource.java
@@ -4,23 +4,31 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.Input;
+import org.springframework.cloud.stream.annotation.Output;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.cloud.stream.app.sftp.source.SftpSourceConfiguration;
 import org.springframework.cloud.stream.messaging.Source;
 import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.messaging.MessageChannel;
 import org.springframework.stereotype.Component;
 
 @Component
-@EnableBinding(Source.class)
-@ImportAutoConfiguration(SftpSourceConfiguration.class)
+@EnableBinding(StockOnHandSftpSource.CustomSource.class)
 public class StockOnHandSftpSource {
     private final Log log = LogFactory.getLog(getClass());
 
 
-    @ServiceActivator(inputChannel = "sftpDataChannel")
+    @StreamListener(CustomSource.STOCK_ON_HAND_RAW)
     public void process(String eventLine) {
         this.log.info("Received line : " + eventLine);
     }
 
 
+    public interface CustomSource {
+        String STOCK_ON_HAND_RAW = "stock-on-hand-raw";
+
+        @Input(STOCK_ON_HAND_RAW)
+        MessageChannel stockOnHandRaw();
+    }
 }
--- src/main/java/com/vinsleo/stockonhand/source/StockOnHandSourceAppApplication.java
+++ src/main/java/com/vinsleo/stockonhand/source/StockOnHandSourceAppApplication.java
@@ -1,10 +1,13 @@
 package com.vinsleo.stockonhand.source;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.cloud.stream.app.sftp.source.SftpSourceConfiguration;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@ImportAutoConfiguration(SftpSourceConfiguration.class)
 public class StockOnHandSourceAppApplication {
 
 	public static void main(String[] args) {
